03a70e81a92127bd9627d15ddd4b4d08863ce2c5
Quazzik <113617617+Quazzik@users.noreply.github.com>
2026-03-30 11:10:26 +0500

    little instruction on index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,8 @@ export default function HomePage() {
       <Title level={2}>Добро пожаловать в мое приложение!</Title>
       <Paragraph>
         <Title level={3}>Это первая страница, созданная с использованием umiJS и Ant Design.</Title>
-        <p>Здесь ничего нет, попробуйте посмотреть раздел "О программе"</p>
+        <p>Здесь так же имеется взаимодействия с  API. Для корректной работы необходимо скачать с <a href="https://github.com/Quazzik/CarAPI">GitHub</a> и запустить сервер на порту 5212 </p>
+        <p>Базовые справочники уже заполнены, находятся в том же репозитории в базе sqlite. Логин/пароль: Login/Password</p>
       </Paragraph>
     </Card>
   );
